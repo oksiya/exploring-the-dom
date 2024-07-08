81d0e12a2992ac98d549c25ba9ff164f1e83b5ac
Siyabonga Masuku <siya.coma@gmail.com>
2024-07-08 09:36:10 +0200

    done

---
+++ index.js
@@ -0,0 +1,22 @@
+const button = document.querySelector('.theMessageButton');
+const myMessage = document.querySelector('.myMessage');
+myMessage.innerText = 'This is a message in the DOM!'
+
+
+button.addEventListener('click', function() {
+    myMessage.classList.toggle('darkmode')
+})
+
+const fruits = ['Apples', 'Pears', 'Oranges', 'Grapes', 'Bananas'];
+
+const fruitList = document.querySelector(".fruits");
+
+for(let i=0;i<fruits.length;i++){
+   // get fruits from the list
+   const fruit = fruits[i];
+   
+   // create a new li element
+   const li = document.createElement('li');
+   li.innerText = fruit;
+   fruitList.appendChild(li);
+}
